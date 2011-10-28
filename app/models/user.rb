d37bc08b2a82819e9bd650d017aba9cefedcd5e5
mamatozay <mamatozay.enine@gmail.com>
2011-10-28 15:57:20 +0800

    Done with sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,8 +29,19 @@ class User < ActiveRecord::Base
     encrypted_password == encrypt(submitted_password)
   end
 
-  private
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil  if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
 
+  def self.authenticate_with_salt(id, cookie_salt)
+    user = find_by_id(id)
+    (user && user.salt == cookie_salt) ? user : nil
+  end
+
+  private
+  
     def encrypt_password
       self.salt = make_salt unless has_password?(password)
       self.encrypted_password = encrypt(password)
@@ -46,5 +57,6 @@ class User < ActiveRecord::Base
 
     def secure_hash(string)
       Digest::SHA2.hexdigest(string)
-    end  
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 SampleApp::Application.routes.draw do
   
-  resources :users
+  #get "sessions/new"
 
+  resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
+  
   root :to => 'pages#home'
   
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
   match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
   
   #get "pages/home"
   #get "pages/contact"
